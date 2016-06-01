6a060eaadfea90faef90104fc43e08890e8d8816
Matt Peretick <mattjp@umich.edu>
2016-06-01 12:34:30 -0400

    slightly more refined

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls import url, patterns
+from django.conf.urls import url
 from . import views
 from django.views.generic import RedirectView
 
-urlpatterns = patterns('',
+urlpatterns = [
 	url(r'^$', RedirectView.as_view(url='/myapp/')),
 	url(r'^myapp/$', views.index, name='index'),
-)
+	url(r'^yourapp/$', views.second, name='second_page'),
+]
