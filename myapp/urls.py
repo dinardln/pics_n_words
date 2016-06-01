59df310d0308a207415ddc989776b130330a6d91
Matt Peretick <mattjp@umich.edu>
2016-06-01 08:56:12 -0400

    added homepage redirection

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,8 @@
-from django.conf.urls import url
+from django.conf.urls import url, patterns
 from . import views
+from django.views.generic import RedirectView
 
-urlpatterns = [
-	url(r'^$', views.index, name='index'),
-]
+urlpatterns = patterns('',
+	url(r'^$', RedirectView.as_view(url='/myapp/')),
+	url(r'^myapp/$', views.index, name='index'),
+)
