fcc4627bc3a9648c58ae02b66095d9d065acfa19
mattjp <mattjp@umich.edu>
2016-06-04 15:58:27 -0400

    removed

--- myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-from django.views.generic import RedirectView
-
-urlpatterns = [
-	url(r'^$', RedirectView.as_view(url='/myapp/')),
-	url(r'^myapp/$', views.index, name='index'),
-	url(r'^yourapp/$', views.second, name='second_page'),
-]
